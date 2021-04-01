44cde02e28b145d33e43733deb1138b092030c7c
Seth Van Meter <70532030+MincraftEinstein@users.noreply.github.com>
2021-04-01 16:37:12 -0400

    Added config for the bounce sound

--- src/main/java/einstein/tbouncepad/TinkersBouncePad.java
+++ src/main/java/einstein/tbouncepad/TinkersBouncePad.java
@@ -7,8 +7,11 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(TinkersBouncePad.MODID)
 public class TinkersBouncePad
@@ -16,11 +19,16 @@ public class TinkersBouncePad
     public static final String MODID = "tbouncepad";
     
     public TinkersBouncePad() {
-    }
+    	ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ModInit.CLIENTSPEC);
+	}
     
     @Mod.EventBusSubscriber(modid = TinkersBouncePad.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ModBlocks
+    public static class ModInit
     {
         public static final Block BOUNCE_PAD = RegistryHandler.registerBlock(TinkersBouncePad.MODID, "bounce_pad", new BouncePad(Block.Properties.create(Material.CLAY, MaterialColor.GRASS).hardnessAndResistance(0.3F).harvestTool(ToolType.SHOVEL).sound(SoundType.SLIME).setAllowsSpawn(Blocks::neverAllowSpawn)), ItemGroup.TRANSPORTATION);
+        
+        public static ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
+        public static final ForgeConfigSpec.BooleanValue BOUNCE_SOUND = CLIENT_BUILDER.comment("If true then a slime sound will play when an entity bounces on the bounce pad").define("bounceSound", true);
+        private static final ForgeConfigSpec CLIENTSPEC = CLIENT_BUILDER.build();
     }
 }
