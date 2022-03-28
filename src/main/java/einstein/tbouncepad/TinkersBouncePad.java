a1efedeab14ab9f74027ba6f56694d5cb9c69597
Seth Van Meter <70532030+MincraftEinstein@users.noreply.github.com>
2022-03-28 15:31:07 -0400

    Updated to 1.18.2
    Einstein's Library is no longer a dependency

--- src/main/java/einstein/tbouncepad/TinkersBouncePad.java
+++ src/main/java/einstein/tbouncepad/TinkersBouncePad.java
@@ -1,7 +1,8 @@
 package einstein.tbouncepad;
 
-import einstein.einsteins_library.util.RegistryHandler;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
@@ -9,11 +10,14 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(TinkersBouncePad.MODID)
 public class TinkersBouncePad
@@ -22,16 +26,26 @@ public class TinkersBouncePad
     public static final String TCON_MODID = "tconstruct";
     
     public TinkersBouncePad() {
+    	final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
     	ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ModInit.CLIENTSPEC);
+    	ModInit.ITEMS.register(modEventBus);
+    	ModInit.BLOCKS.register(modEventBus);
 	}
     
-    @EventBusSubscriber(modid = TinkersBouncePad.MODID, bus = Bus.MOD)
     public static class ModInit
     {
-        public static final Block BOUNCE_PAD = RegistryHandler.registerBlock(TinkersBouncePad.MODID, "bounce_pad", new BouncePad(BlockBehaviour.Properties.of(Material.CLAY, MaterialColor.GRASS).strength(0.3F).sound(SoundType.SLIME_BLOCK).isValidSpawn(Blocks::never)), CreativeModeTab.TAB_TRANSPORTATION);
-        public static final Block SKYSLIME_BOUNCE_PAD = RegistryHandler.registerBlock(TinkersBouncePad.MODID, TinkersBouncePad.TCON_MODID, "skyslime_bounce_pad", new BouncePad(BlockBehaviour.Properties.copy(ModInit.BOUNCE_PAD)), CreativeModeTab.TAB_TRANSPORTATION);
-        public static final Block ENDERSLIME_BOUNCE_PAD = RegistryHandler.registerBlock(TinkersBouncePad.MODID, TinkersBouncePad.TCON_MODID, "enderslime_bounce_pad", new BouncePad(BlockBehaviour.Properties.copy(ModInit.BOUNCE_PAD)), CreativeModeTab.TAB_TRANSPORTATION);
-        public static final Block ICHOR_BOUNCE_PAD = RegistryHandler.registerBlock(TinkersBouncePad.MODID, TinkersBouncePad.TCON_MODID, "ichor_bounce_pad", new BouncePad(BlockBehaviour.Properties.copy(ModInit.BOUNCE_PAD)), CreativeModeTab.TAB_TRANSPORTATION);
+    	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    	private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    	
+        public static final RegistryObject<Block> BOUNCE_PAD = BLOCKS.register("bounce_pad", () -> new BouncePad(BlockBehaviour.Properties.of(Material.CLAY, MaterialColor.GRASS).strength(0.3F).sound(SoundType.SLIME_BLOCK).isValidSpawn(Blocks::never)));
+        public static final RegistryObject<Block> SKYSLIME_BOUNCE_PAD = BLOCKS.register("skyslime_bounce_pad", () -> new BouncePad(BlockBehaviour.Properties.copy(ModInit.BOUNCE_PAD.get())));
+        public static final RegistryObject<Block> ENDERSLIME_BOUNCE_PAD = BLOCKS.register("enderslime_bounce_pad", () -> new BouncePad(BlockBehaviour.Properties.copy(ModInit.BOUNCE_PAD.get())));
+        public static final RegistryObject<Block> ICHOR_BOUNCE_PAD = BLOCKS.register("ichor_bounce_pad", () -> new BouncePad(BlockBehaviour.Properties.copy(ModInit.BOUNCE_PAD.get())));
+        
+        public static final RegistryObject<Item> BOUCE_PAD_ITEM = ITEMS.register("bounce_pad", () -> new BlockItem(BOUNCE_PAD.get(), new Item.Properties().stacksTo(16).tab(CreativeModeTab.TAB_TRANSPORTATION)));
+        public static final RegistryObject<Item> SKYSLIME_PAD_ITEM = ITEMS.register("skyslime_bounce_pad", () -> new BlockItem(SKYSLIME_BOUNCE_PAD.get(), new Item.Properties().stacksTo(16).tab(CreativeModeTab.TAB_TRANSPORTATION)));
+        public static final RegistryObject<Item> ENDERSLIME_BOUCE_PAD_ITEM = ITEMS.register("enderslime_bounce_pad", () -> new BlockItem(ENDERSLIME_BOUNCE_PAD.get(), new Item.Properties().stacksTo(16).tab(CreativeModeTab.TAB_TRANSPORTATION)));
+        public static final RegistryObject<Item> ICHOR_PAD_ITEM = ITEMS.register("ichor_bounce_pad", () -> new BlockItem(ICHOR_BOUNCE_PAD.get(), new Item.Properties().stacksTo(16).tab(CreativeModeTab.TAB_TRANSPORTATION)));
         
         public static ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
         public static final ForgeConfigSpec.BooleanValue BOUNCE_SOUND = CLIENT_BUILDER.comment("If true then a slime sound will be played when an entity bounces on the bounce pad").define("bounceSound", true);
